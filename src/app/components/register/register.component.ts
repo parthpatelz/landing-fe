fc5500e21f5efc06965b96005750ccf29e2e78ea
Dumindu Harshana <ruvinduharshana536@gmail.com>
2024-06-13 02:31:19 -0400

    Add files via upload

---
+++ src/app/components/register/register.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { JwtService } from 'src/app/service/jwt.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+
+  registerForm: FormGroup | undefined;
+
+  constructor(
+    private service: JwtService,
+    private fb: FormBuilder
+  ) { }
+
+  ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      fullName: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    })
+  }
+
+
+  submitForm() {
+    console.log(this.registerForm.value);
+    this.service.register(this.registerForm.value).subscribe(
+      (response) => {
+        if (response.id != null) {
+          alert("Hello " + response.fullName);
+        }
+      }
+    )
+  }
+
+}
